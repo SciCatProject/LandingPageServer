131caa41dea8f0e2949193a582f08b3cf66f3907
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-16 14:57:06 +0200

    🐛

--- src/app/dataset.ts
+++ src/app/dataset.ts
@@ -2,4 +2,10 @@ export class Dataset {
   id: number;
   name: string;
   creator: string;
+  affiliation: string;
+  title: string;
+  publisher: string;
+  year: string;
+  url: string;
+  resource_type: string;
 }
