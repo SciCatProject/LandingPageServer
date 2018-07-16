87f1a77c3ae28530fccb12e6ff8ea8b76302d108
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-16 10:20:16 +0200

    💡

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: '10.1799/ESS0001', url: 'http://esss.se' },
+      { id: 12, name: '10.1799/ESS0002' },
+      { id: 13, name: '10.1799/ESS0003' },
+      { id: 14, name: '10.1799/ESS0004' },
+      { id: 15, name: '10.1799/ESS0005' },
+      { id: 16, name: '10.1799/ESS0006' },
+      { id: 17, name: '10.1799/ESS0007' },
+      { id: 18, name: '10.1799/ESS0008' },
+      { id: 19, name: '10.1799/ESS0009' },
+      { id: 20, name: '10.1799/ESS0010' }
     ];
     return {heroes};
   }
