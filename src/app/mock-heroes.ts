956a1a41d549e5edab5172aee981b4fe49c64a0e
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-16 14:57:07 +0200

    📇

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,114 @@
-import { Dataset } from './dataset';
+import {Dataset} from './dataset';
 
 export const HEROES: Dataset[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  {
+    id: 11,
+    name: 'https://doi.org/10.1799/ESS0001',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 12,
+    name: 'https://doi.org/10.1799/ESS0002',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 13,
+    name: 'https://doi.org/10.1799/ESS0003',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 14,
+    name: 'https://doi.org/10.1799/ESS0004',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 15,
+    name: 'https://doi.org/10.1799/ESS0005',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 16,
+    name: 'https://doi.org/10.1799/ESS0006',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 17,
+    name: 'https://doi.org/10.1799/ESS0007',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 18,
+    name: 'https://doi.org/10.1799/ESS0008',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 19,
+    name: 'https://doi.org/10.1799/ESS0009',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS Detector Group',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  },
+  {
+    id: 20,
+    name: 'https://doi.org/10.1799/ESS0010',
+    creator: 'Dorothea Pfeiffer',
+    affiliation: 'ESS DG',
+    title: 'Vanadium sample data from NMX beam line',
+    publisher: 'ESS-DMSC',
+    year: '2017',
+    url: 'https://doi.org/10.1799/ESS0001',
+    resource_type: 'NeXus HDF5 file'
+  }
 ];
