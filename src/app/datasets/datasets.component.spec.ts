4dea1b6ad63a5d4a20b00accfd3f66473d24ddb9
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-19 12:13:41 +0200

    📚

--- src/app/datasets/datasets.component.spec.ts
+++ src/app/datasets/datasets.component.spec.ts
@@ -2,13 +2,19 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DatasetsComponent} from './datasets.component';
 
+import {RouterTestingModule} from '@angular/router/testing';
+import {DatasetService} from '../dataset.service';
+import {DATASETS} from '../mock-datasets';
+
 describe('DatasetsComponent', () => {
   let component: DatasetsComponent;
   let fixture: ComponentFixture<DatasetsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DatasetsComponent]
+      imports: [RouterTestingModule],
+      declarations: [DatasetsComponent],
+      providers: [{provide: DatasetService, use: DATASETS}]
     })
       .compileComponents();
   }));
