afd8891731ee7641597c6d8c3413e69e239ec3e8
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-19 10:20:14 +0200

    📇

--- src/app/dataset.service.ts
+++ src/app/dataset.service.ts
@@ -1,31 +1,31 @@
-import { Injectable, Inject, Optional } from '@angular/core';
-import { APP_BASE_HREF } from '@angular/common';
-import { HttpClient, HttpHeaders }from '@angular/common/http';
+import {Inject, Injectable, Optional} from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
 
-import { Dataset } from './dataset';
-import { MessageService } from './message.service';
+import {Dataset} from './dataset';
+import {MessageService} from './message.service';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
 @Injectable()
 export class DatasetService {
 
-  private heroesUrl = 'api/heroes';  // URL to web api
+  private readonly heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService,
     @Optional() @Inject(APP_BASE_HREF) origin: string) {
-      this.heroesUrl = `${origin}${this.heroesUrl}`;
-    }
+    this.heroesUrl = `${origin}${this.heroesUrl}`;
+  }
 
   /** GET heroes from the server */
-  getHeroes (): Observable<Dataset[]> {
+  getHeroes(): Observable<Dataset[]> {
     return this.http.get<Dataset[]>(this.heroesUrl)
       .pipe(
         tap(heroes => this.log('fetched datasets')),
@@ -71,8 +71,8 @@ export class DatasetService {
   //////// Save methods //////////
 
   /** POST: add a new dataset to the server */
-  addHero (name: string): Observable<Dataset> {
-    const hero = { name };
+  addHero(name: string): Observable<Dataset> {
+    const hero = {name};
 
     return this.http.post<Dataset>(this.heroesUrl, hero, httpOptions).pipe(
       tap((hero: Dataset) => this.log(`added hero w/ id=${hero.id}`)),
@@ -81,7 +81,7 @@ export class DatasetService {
   }
 
   /** DELETE: delete the dataset from the server */
-  deleteHero (hero: Dataset | number): Observable<Dataset> {
+  deleteHero(hero: Dataset | number): Observable<Dataset> {
     const id = typeof hero === 'number' ? hero : hero.id;
     const url = `${this.heroesUrl}/${id}`;
 
@@ -92,7 +92,7 @@ export class DatasetService {
   }
 
   /** PUT: update the dataset on the server */
-  updateHero (hero: Dataset): Observable<any> {
+  updateHero(hero: Dataset): Observable<any> {
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
@@ -105,7 +105,7 @@ export class DatasetService {
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
    */
-  private handleError<T> (operation = 'operation', result?: T) {
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
       // TODO: send the error to remote logging infrastructure
