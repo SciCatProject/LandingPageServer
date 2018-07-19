50b84da881a4a6f97f15214283bc59a95996b284
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-19 14:20:57 +0200

    📚

--- src/app/datasets/datasets.component.spec.ts
+++ src/app/datasets/datasets.component.spec.ts
@@ -3,8 +3,12 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {DatasetsComponent} from './datasets.component';
 
 import {RouterTestingModule} from '@angular/router/testing';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from '../in-memory-data.service';
+import {MockDatasetService} from '../MockStubs';
 import {DatasetService} from '../dataset.service';
-import {DATASETS} from '../mock-datasets';
+import {Mock} from 'protractor/built/driverProviders';
+
 
 describe('DatasetsComponent', () => {
   let component: DatasetsComponent;
@@ -12,11 +16,13 @@ describe('DatasetsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
+      ],
       declarations: [DatasetsComponent],
-      providers: [{provide: DatasetService, use: DATASETS}]
-    })
-      .compileComponents();
+      providers: [{provide: DatasetService , useClass: MockDatasetService}]
+    });
+    TestBed.compileComponents();
   }));
 
   beforeEach(() => {
