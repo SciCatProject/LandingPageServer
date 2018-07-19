8d7d0d87c4868f3c4f79f7f2bdb90d852a4ed4ea
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-19 10:29:31 +0200

    📇

--- src/app/dataset-search/dataset-search.component.spec.ts
+++ src/app/dataset-search/dataset-search.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DatasetSearchComponent } from './dataset-search.component';
+import {DatasetSearchComponent} from './dataset-search.component';
 
 describe('DatasetSearchComponent', () => {
   let component: DatasetSearchComponent;
@@ -8,9 +8,9 @@ describe('DatasetSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DatasetSearchComponent ]
+      declarations: [DatasetSearchComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
