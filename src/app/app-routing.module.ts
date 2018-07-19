0cec23395fb666f2249c9469e148ed38b404d5f8
Gareth Murphy <garethcmurphy@gmail.com>
2018-07-19 10:20:13 +0200

    🔖

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { DatasetsComponent }      from './heroes/datasets.component';
-import { DatasetDetailComponent }  from './dataset-detail/dataset-detail.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {DatasetsComponent} from './heroes/datasets.component';
+import {DatasetDetailComponent} from './dataset-detail/dataset-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: DatasetDetailComponent },
-  { path: 'heroes', component: DatasetsComponent }
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: DatasetDetailComponent},
+  {path: 'heroes', component: DatasetsComponent}
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
