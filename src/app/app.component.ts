b61f72968998cce0d67c9b447ec4a2640830d791
Gareth Murphy <garethcmurphy@gmail.com>
2018-08-10 15:04:21 +0200

    Title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ESS Open Access Repository';
+  title = 'ESS Public Data Repository';
 }
