af838d401f76a5b70e0cb4835c86141ce9c83f36
Gareth Murphy <garethcmurphy@gmail.com>
2019-06-15 06:39:22 +0200

    fix unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+	browsers: ['ChromeHeadless', 'Chrome'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     singleRun: false
   });
 };
