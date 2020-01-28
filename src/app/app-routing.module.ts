6760d611c159d7e0ee933c5ca91510c18baa33cc
Henrik Johansson <henrik.johansson712@gmail.com>
2020-01-28 16:52:53 +0100

    Fixed routes conditional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,20 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { DatasetDetailComponent } from "./dataset-detail/dataset-detail.component";
+import { APP_DI_CONFIG } from "./app-config.module";
+import { PublishedDataDetailComponent } from "./publisheddata-detail/publisheddata-detail.component";
+
+const getComponent = () => {
+  if (APP_DI_CONFIG.useMaterialView) {
+    return DatasetDetailComponent;
+  } else {
+    return PublishedDataDetailComponent;
+  }
+};
 
 const routes: Routes = [
-  { path: "^/detail/:id(.*//.*)", component: DatasetDetailComponent },
-  { path: "detail/:id", component: DatasetDetailComponent },
+  { path: "^/detail/:id(.*//.*)", component: getComponent() },
+  { path: "detail/:id", component: getComponent() },
   { path: "", component: DashboardComponent }
 ];
 
