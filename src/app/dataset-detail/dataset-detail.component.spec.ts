9dac54da8cab3a3c0c8d39f86ecc5b214a6143a9
Luke Gorman <luke.gorman@psi.ch>
2020-01-30 16:16:30 +0000

    include OAI service in test

--- src/app/dataset-detail/dataset-detail.component.spec.ts
+++ src/app/dataset-detail/dataset-detail.component.spec.ts
@@ -2,11 +2,13 @@ import { DatasetDetailComponent } from "./dataset-detail.component";
 import { DatasetService } from "../dataset.service";
 import { FileSizePipe } from "../filesize.pipe";
 import { MatCardModule } from "@angular/material/card";
-import { MockDatasetService, MockActivatedRoute } from "../MockStubs";
+import { MockDatasetService, MockActivatedRoute, MockHttp, MockOAIervice } from "../MockStubs";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { MatListModule } from "@angular/material/list";
 import { ActivatedRoute } from "@angular/router";
 import { APP_CONFIG } from "../app-config.module";
+import { HttpClient } from "@angular/common/http";
+import { OAIService } from "../oai.service";
 
 describe("DatasetDetailComponent", () => {
   let component: DatasetDetailComponent;
@@ -17,6 +19,8 @@ describe("DatasetDetailComponent", () => {
       imports: [MatCardModule, MatListModule],
       declarations: [DatasetDetailComponent, FileSizePipe],
       providers: [
+        { provide: OAIService, useClass: MockOAIervice },
+        { provide: HttpClient, useClass: MockHttp },
         { provide: DatasetService, useClass: MockDatasetService },
         { provide: ActivatedRoute, useClass: MockActivatedRoute },
         {
