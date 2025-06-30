8b701fe53f015493df46465a91f5a39c4e50b8ff
Omkar Zade <omkar.zade@psi.ch>
2025-06-30 15:06:03 +0200

    Remove unused variables from app-config.module.ts
    They were added because of the rebase. But now these variables have
    moved to app-config.service.ts

--- src/app/app-config.module.ts
+++ src/app/app-config.module.ts
@@ -5,37 +5,12 @@ export const APP_CONFIG = new InjectionToken<AppConfig>("app.config");
 
 export class AppConfig {
   production = true;
-  facility = "";
-  oaiProviderRoute = "";
-  doiBaseUrl = "";
-  directMongoAccess = true;
-  accessDataHref = "";
-  accessInstructions = "";
-  scicatBaseUrl = "";
-  showLogoBanner = true;
-  retrieveToEmail:
-    | {
-        title: string;
-        option: string;
-        username: string;
-        confirmMessage?: string;
-      }
-    | undefined = undefined;
   statusMessage = "";
   statusCode: "INFO" | "WARN" | "NONE" = "NONE";
 }
 
 export const APP_DI_CONFIG: AppConfig = {
   production: environment.production,
-  facility: environment.facility ?? "",
-  oaiProviderRoute: environment.oaiProviderRoute ?? "",
-  doiBaseUrl: environment.doiBaseUrl ?? "",
-  directMongoAccess: environment.directMongoAccess ?? false,
-  accessDataHref: environment.accessDataHref ?? "",
-  accessInstructions: environment.accessInstructions ?? "",
-  scicatBaseUrl: environment.scicatBaseUrl ?? "",
-  showLogoBanner: environment.showLogoBanner ?? false,
-  retrieveToEmail: environment["retrieveToEmail"] ?? undefined,
   statusMessage: environment["statusMessage"] || "",
   statusCode: (["INFO", "WARN", "NONE"].includes(environment["statusCode"])
     ? environment["statusCode"]
